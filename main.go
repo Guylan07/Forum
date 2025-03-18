aa727fe5d840244997b06253255813badb3968c5
rguylan <guylan.richaud@ynov.com>
2025-03-18 12:36:42 +0100

    port8081,explication

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
+import ( //Les importation de packages
+	"fmt" // Sert à afficher du texte et des réponses HTTP
+	"net/http" //Fournit des fonctionnalités nécessaires pour créer un serveur web.
+
 )
 
 func main () {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Bienvenue sur mon forum !")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //Associe une route HTTP (ça /) à une fonction qui gère les 
+	// requêtes des utilisateurs (en gros quand un utioisateur accède à http://localhost:8081/, la fonction associée est éxécutée)
+		fmt.Fprint(w, "Bienvenue sur mon forum !") //
 	})
 	
-	port := "8081"
-	fmt.Printf("Lancement du serveur sur le port 8081", port)
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		fmt.Printf("Pas de démarrage le serveur bug", err)
+	port := "8081" //Port d'écoute définit 8081
+	fmt.Printf("Lancement du serveur sur le port %s\n", port)
+	err := http.ListenAndServe(":"+port, nil) //Lancement du serveur
+	if err != nil { //Gestion des erreurs
+		fmt.Printf("Pas de démarrage le serveur bug : %s\n", err)
 	}
 }
