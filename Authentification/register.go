91d071b345ca03d99b01676bf8a5b5aaf04fa1ba
rguylan <guylan.richaud@ynov.com>
2025-03-28 09:52:53 +0100

    t

--- Authentification/register.go
+++ Authentification/register.go
@@ -1,1 +1,18 @@
 package authentification
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		fmt.Printf("Enregistrement réussi : %s\n,", username)
+		fmt.Fprintln(w, "Utilisateur enregistré avec succès")
+	} else {
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+	}
+}
