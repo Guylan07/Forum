58319a7f14782468778c5979b1bb4be9f6dc09a6
rguylan <guylan.richaud@ynov.com>
2025-04-07 10:25:06 +0200

    css

--- forum/cmd/main.go
+++ forum/cmd/main.go
@@ -66,7 +66,7 @@ func main() {
 	
 	// On démarre le serveur sur le port 8080
 	// C'est comme ouvrir officiellement les portes du bâtiment et commencer à accueillir les visiteurs
-	log.Println("Starting server on :8080...")
+	log.Println("Starting server on :8085...")
 	// Si le serveur rencontre une erreur fatale, on enregistre l'erreur
-	log.Fatal(http.ListenAndServe(":8080", authMiddleware))
+	log.Fatal(http.ListenAndServe(":8085", authMiddleware))
 }
